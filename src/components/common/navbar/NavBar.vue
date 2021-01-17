2bdfe9b890d1c7d1e2ead57531507f14d556ab1f
ZWN101 <zwn5350@163.com>
2021-01-17 19:18:54 +0800

    第二次提交

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navBar">
+      <div class="left"><slot name="left"></slot></div>
+      <div class="content"><slot name="content"></slot></div>
+      <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navBar{
+    width: 100%;
+    height: 44px;
+    line-height: 44px;
+    display: flex;
+    text-align: center;
+}
+.left,.right{
+    width: 60px;
+}
+.content{
+    flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-      <h2>首页</h2>
+      <nav-bar class="homeNav">
+        <div slot="content">购物街</div>
+      </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar.vue'
 export default {
+  components: { NavBar },
     name:'Home'
 }
 </script>
 
 <style>
-
+.homeNav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
