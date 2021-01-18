154aee2933282a20b71341ce364af18da395d659
ZWN101 <zwn5350@163.com>
2021-01-18 17:06:51 +0800

    0118

---
+++ src/components/common/tabControl/TabControl.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tabControl">
+      <div v-for="(item,index) in titles" class="tabControlItem" @click="itemClick(index)" >
+          <span :class="{active:index==currentIndex}">{{item}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'TabControl',
+    data(){
+        return{
+            currentIndex:0
+        }
+    },
+    props:{
+        titles:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    },
+    methods:{
+        itemClick(index){
+            // console.log(index);
+            this.currentIndex=index
+        }
+    }
+}
+</script>
+
+<style>
+.tabControl{
+    display: flex;
+    text-align: center;
+    background-color: #fff;
+    height: 40px;
+    line-height: 40px;
+    font-size: 14px;
+}
+.tabControlItem{
+    flex: 1;
+}
+.active{
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,28 @@
 <template>
-  <div>
+  <div class="home">
       <nav-bar class="homeNav">
         <div slot="content">购物街</div>
       </nav-bar>
       <home-swiper :banners="banners"></home-swiper>
       <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control :titles="['流行','新款','精选']"></tab-control>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar.vue'
 import HomeSwiper from './childComps/HomeSwiper.vue';
+import RecommendView from './childComps/RecommendView.vue';
+import FeatureView from './childComps/FeatureView.vue';
+import TabControl from '../../components/common/tabControl/TabControl.vue';
+
 
 import {getHomeMultidata} from 'network/home';
-import RecommendView from './childComps/RecommendView.vue';
 
 export default {
   name:'Home',
-  components: {NavBar,HomeSwiper, RecommendView},
+  components: {NavBar,HomeSwiper, RecommendView, FeatureView, TabControl},
   data(){
     return {
       banners:[],
@@ -36,8 +41,17 @@ export default {
 </script>
 
 <style>
+.home{
+  padding-top: 44px;
+  padding-bottom: 400px;
+}
 .homeNav{
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  right: 0px;
+  z-index: 10;
 }
 </style>
